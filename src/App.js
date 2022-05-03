729526d27e5912ac182a18c002f6c76c688a3993
JawaraGordon <jawaragordon@icloud.com>
2022-05-03 19:21:50 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
+import ProjectForm from "./ProjectForm";
+import ProjectList from "./ProjectList";
+import projects from "./ProjectData"
 
 function App() {
+  console.log(projects)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <ProjectForm />
+      <ProjectList projects={projects} />
     </div>
-  );
+  )
 }
 
 export default App;
